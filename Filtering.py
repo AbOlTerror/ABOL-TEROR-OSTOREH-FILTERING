ba81cae993f0482b2f0dda4f92e8d7ae6d1295cb
ABOL TEROR OSTOREH FILTERING <110899088+AbOlTerror@users.noreply.github.com>
2022-08-10 10:01:15 +0430

    rm -rf cod-filtering-keristofer  git clone https://github.com/AbOlTerror cd cod-filtering-AbOlTerror chmod 777 *  python cod-filtering-AbOlTerror.py

---
+++ Filtering.py
@@ -0,0 +1,145 @@
+‌
+print("A B O L")
+print(" teror ")
+print("  ostoreh filtering  ")
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time
+import sys
+import colored
+import colorama
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+print(f"{red} ")
+print(Fore.WHITE + "")
+#date
+x = str(datetime.datetime.now())
+print("started " + (x))
+print(f"{green} ")
+print(" cod filtering ")
+print(Fore.YELLOW + "")
+x = """ Loading ~ ~ ~
+n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb                              dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+ 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+"""
+for c in x:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.002)
+print(f"{blue} ")
+print("#.........")
+time.sleep(0.9)
+print("##........")
+time.sleep(0.9)
+print("###.......")
+time.sleep(0.8)
+print("####......")
+time.sleep(0.7)
+print("#####.....")
+time.sleep(0.6)
+print("######....")
+time.sleep(0.5)
+print("#######...")
+time.sleep(0.4)
+print("########..")
+time.sleep(0.3)
+print("#########.")
+time.sleep(0.2)
+print("##########")
+time.sleep(0.1)
+print(f"{pink} ")
+print("Play - - - ! ")
+print(Fore.YELLOW + "")
+time.sleep(1.7)
+print("  ostoreh bozorg filtering (teror) ")
+print(" ")
+print("   yadegari rubika = @sazndeh_cFc  ")
+print(" ")
+print(f"{green} ")
+print(" cod server ")
+time.sleep(0.1)
+print("server1 = y//d/f")
+time.sleep(1.0)
+print("server2 = g/d//")
+time.sleep(0.1)
+print(" Dxprit ")
+time.sleep(1.0)
+print("Dxprit = http://Dxprit.ngopb.hackers65.7678729986.infect.in ")
+time.sleep(0.1)
+print(f"{red} ")
+up = input("""
+《 cod filter account 1 》
+《 cod filter gap 2 》
+《 cod filter chanel 3 》
+ Which do you want >>> """)
+print("  ")
+print(f"{blue} ")
+time.sleep(2.0)
+if up == "1":
+        print(' cod account = (' https://rubika.ir/filter.play/www.photo.sex.com/2861274238378901110./5.5.0.3fil_accunt=git_txt_sex_xnxx&porn.17.16.32.08.000com=filter.accunt.rubika=(/https://bit.ly/3ild93L.fa*.af*.fa*.)(17.16.32.09.000)(yftt16k).(Filtering-Acoount)=filter.servr.rubika.com).af.*fa.*af.*fa.*af.*fa.*af.*fa.*.com ')
+if up != "1":
+        print(' cod filter account ')
+if up == "2":
+        print(' cod gap = (https://5.106.8.151/(80.0.3987.99).Badge.https://www.xvideos.com/video60584481/teensex_xxxdial.TextBlue3-14839830 https://s4.uupload.ir/files/img_20210728_194732_694_vlkn.jpg.GetFile.ashx.(Rubika.source./M1.00002881,1.127.000:8080).site.Against.the.law.PINGpornhub.com(10.10.34.36)56.https://s4.uupload.ir/files/img_20210728_194732_694_vlkn.jpg://runtime-es2015.80.Filter.Rubika.accont)0.3987.998cbc97194c7.application/x-www-form-urlencoded.ir.resaneh1Filteri.Rubika).*fill-*rubika*f*fill-xxx(Gif.sex) ')
+if up != "2":
+        print(' cod filter gap ')
+if up == "3":
+        print(' cod chanel = https://filter.filter.filtering.chanel-rubika.filtering.com(photo.gif.sxs.com/yfttk15s/)1.2.5.7.9.0.2.1.4.6.8.0.e.4.r.i.67.n.g.1.co.fil.a.h.x.g.fil.sex ')
+if up != "3":
+        print(' cod filter chanel ')
+time.sleep(0.6)
+print("""
+         cod haro mahklot konid !
+""")
+print(f"{pink} ")
+print(" cod ro 15 bar bezanid ke feik nashe ")
+print("♡Thanks♡")
+print(" ip rubika = 127.0.0.1 ")
+print(f"{red} ")
+print(" no love ")
+print(" ")
+print(" ostoreh bozorg filtering aqa abol ( teror ) ")
+time.sleep(2.0)
+print(""" Update Kardan script 👇:
+    	
+    	
+rm -rf cod-filtering-keristofer 
+git clone https://github.com/AbOlTerror
+cd cod-filtering-AbOlTerror
+chmod 777 * 
+python cod-filtering-AbOlTerror.py
+""")
+print(" ")
+print(" ostoreh filtering ")
+print(" abol teror ")
+print(" the end ")
+time.sleep(2.0)
